6bb0440d830d8ecb3dbe92c2a78b77bbb93a3748
Katherine Hambley <superdesigngirl@mac.com>
2022-12-20 14:39:12 -0600

    Added MeterSearchPage

--- CompassMobileUpdate/CompassMobileUpdate/Pages/MainPage.xaml.cs
+++ CompassMobileUpdate/CompassMobileUpdate/Pages/MainPage.xaml.cs
@@ -11,11 +11,33 @@ namespace CompassMobileUpdate
 {
     public partial class MainPage : ContentPage
     {
+        private double width;
+        private double height;
+        
         public MainPage(MainViewModel viewModel)
         {
             InitializeComponent();
             viewModel.Navigation = Navigation;
             BindingContext = viewModel;
+        }
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            if(width != this.width || height != this.height)
+            {
+                this.width = width;
+                this.height = height;
+                if(width > height)
+                {
+                    outerStack.Orientation = StackOrientation.Horizontal;
+                    outerStack.HorizontalOptions = LayoutOptions.CenterAndExpand;
+                }
+                else
+                {
+                    outerStack.Orientation = StackOrientation.Vertical;
+                }
+            }
         }
     }
 }
---
+++ CompassMobileUpdate/CompassMobileUpdate/Pages/MeterSearchPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CompassMobileUpdate.Pages
+{	
+	public partial class MeterSearchPage : ContentPage
+	{	
+		public MeterSearchPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
