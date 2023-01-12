ee0802a4badf66b03648e24fbc486105e02c371a
c971939 <C971939@exelonds.com>
2023-01-12 13:32:54 -0600

    Added SearchBar, message, lvSearchResults controls to MapSearchPage

--- CompassMobileUpdate/CompassMobileUpdate/ViewModels/MapSearchViewModel.cs
+++ CompassMobileUpdate/CompassMobileUpdate/ViewModels/MapSearchViewModel.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
 namespace CompassMobileUpdate.ViewModels
 {
 	public class MapSearchViewModel : ViewModelBase
 	{
-		public MapSearchViewModel()
+		public ICommand PerformSearch => new Command(async () =>
 		{
-		}
+            //popup for testing purposes
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                App.Current.MainPage.DisplayAlert("PerformMapSearch method test", "The PerformCustomerSearch method was called successfully", "OK");
+            });
+        });
 	}
 }
 
