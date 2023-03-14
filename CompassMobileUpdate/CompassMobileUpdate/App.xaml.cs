4abd677e4f68e5e90b88056fe8d06541b3055a35
c971939 <C971939@exelonds.com>
2023-03-14 12:38:32 -0500

    finished App.cs (root)

--- CompassMobileUpdate/CompassMobileUpdate/App.xaml.cs
+++ CompassMobileUpdate/CompassMobileUpdate/App.xaml.cs
@@ -11,30 +11,39 @@ namespace CompassMobileUpdate
         bool JWTIsExpired = false;
         public App ()
         {
-            AppVariables.StartTime = DateTimeOffset.Now;
-            AppVariables.Application = this;
-            InitializeComponent();
-
-            var RootPage = new NavigationPage(Resolver.Resolve<MainPage>());
-            RootPage.BarBackgroundColor = Color.FromHex("#CC0033");
-            RootPage.BarTextColor = Color.White;
-
-            var appUser = AppVariables.LocalAppSql.GetAppUser();
-            AppVariables.User = appUser;
-            CheckJWTExpiration();
-
-            if (AppVariables.User == null || JWTIsExpired)
+            try
             {
-                var loginPage = Resolver.Resolve<LoginPage>();
-                MainPage = loginPage;
+                AppVariables.StartTime = DateTimeOffset.Now;
+                AppVariables.Application = this;
+                InitializeComponent();
+
+                var RootPage = new NavigationPage(Resolver.Resolve<MainPage>());
+                RootPage.BarBackgroundColor = Color.FromHex("#CC0033");
+                RootPage.BarTextColor = Color.White;
+
+                var appUser = AppVariables.LocalAppSql.GetAppUser();
+                AppVariables.User = appUser;
+                CheckJWTExpiration();
+
+                if (AppVariables.User == null || JWTIsExpired)
+                {
+                    var loginPage = Resolver.Resolve<LoginPage>();
+                    MainPage = loginPage;
+                }
+                else
+                {
+                    Task.Run(async () => await AppVariables.ResetVoltageRules(false));
+                    MainPage = RootPage;
+                }
             }
-            else
+            catch (Exception e)
             {
-                Task.Run(async () => await AppVariables.ResetVoltageRules(false));
-            }
-                MainPage = RootPage;
-            
+                string message = e.Message;
+                string stackTrace = e.StackTrace;
+                throw;
+            }        
         }
+
         private void CheckJWTExpiration()
         {
             if (AppVariables.User != null)
@@ -58,6 +67,7 @@ namespace CompassMobileUpdate
 
         protected override void OnResume ()
         {
+            CheckJWTExpiration();
         }
     }
 }
