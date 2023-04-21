ff26af2c250d22b150461e1eb2897db59a15f807
c971939 <C971939@exelonds.com>
2023-04-21 17:28:14 -0500

    [ScannerPage] Wired up new scanner library

--- CompassMobileUpdate/CompassMobileUpdate/Pages/ScannerPage.xaml.cs
+++ CompassMobileUpdate/CompassMobileUpdate/Pages/ScannerPage.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+using CompassMobileUpdate.Exceptions;
+using CompassMobileUpdate.Models;
+using CompassMobileUpdate.ViewModels;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
@@ -8,9 +12,17 @@ namespace CompassMobileUpdate.Pages
 {
     public partial class ScannerPage : BasePage
     {
-        public ScannerPage()
+        ScannerViewModel vm => BindingContext as ScannerViewModel;
+
+        public string DeviceUtilityID { get; set; }
+
+        public Meter Meter { get; set; }
+
+        public ScannerPage(ScannerViewModel viewModel)
         {
             InitializeComponent();
+            viewModel.Navigation = Navigation;
+            BindingContext = viewModel;
 
             zxingScanner.Options.CameraResolutionPreset = ZXing.Mobile.CameraResolutionPreset.Preset1280x720;
 
@@ -29,9 +41,108 @@ namespace CompassMobileUpdate.Pages
                 // Show an alert
                 await DisplayAlert("Scanned Barcode", result.Text, "OK");
 
-                // Navigate away
-                await Navigation.PopAsync();
+                var meterId = await GetMeterIDFromScanAsync(result.Text);
+
+                if(meterId != null)
+                {
+                    await SetMeterByDeviceUtilityIDAsync(meterId);
+                    NavigateToMeterDetailAsync();
+                }
+
             });
+        }
+
+        private async Task SetMeterByDeviceUtilityIDAsync(string meterId)
+        {
+            var meter = new Meter();
+            meterId = "G270146674"; // Integration testing purposes
+            this.Meter = await vm.GetMeterByDeviceUtilityIDAsync(meterId);          
+        }
+
+        private async void NavigateToMeterDetailAsync()
+        {
+            if(this.Meter == null)
+            {
+                return;
+            }
+            var meterDetailpage = Resolver.Resolve<MeterDetailPage>();
+
+            var detailViewModel = meterDetailpage.BindingContext as MeterDetailViewModel;
+
+            detailViewModel.MeterItem = this.Meter;
+
+            await detailViewModel.GetAllMeterInfo();
+
+            var localSql = new LocalSql();
+
+            await localSql.AddOrUpdateMeterLastAccessedTime(this.Meter);
+
+            await Navigation.PushAsync(meterDetailpage);
+        }
+
+        private async Task<string> GetMeterIDFromScanAsync(string result)
+        {
+            try
+            {
+                if (result == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    aiScannerPage.IsRunning = false;
+
+                    var barcode = result.Trim();
+
+                    int length = barcode.Length;
+
+                    int idLength = 10;
+
+                    var tempBarCode = "";
+
+                    bool tryAgain = false;
+
+                    SerialNumberFormatException ex;
+
+                    for (int i = 0; i < length - 9; i++)
+                    {
+                        if (i + idLength > length)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            //tempBarCode = barcode.Substring(i, idLength);
+
+                            tempBarCode = barcode.Substring(barcode.Length - 10, 10);
+
+                            return tempBarCode;
+                            //if (vm.IsValidSerialNumber(tempBarCode, out ex))
+                            //{
+                            //    return tempBarCode;
+                            //}
+                        }
+                    }
+
+                    tryAgain = await DisplayAlert("Format Exception", "Invalid MeterID or Partial Scan: " + result, "Scan Again", "Cancel");
+
+                    aiScannerPage.IsRunning = true;
+
+                    if (tryAgain)
+                    {
+                        return await GetMeterIDFromScanAsync(result);
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {   
+                await DisplayAlert("Error", "An error occured " + ex.Message, "Close");
+                throw;
+            }
         }
 
         void overlay_FlashButtonClicked(Xamarin.Forms.Button sender, System.EventArgs e)
---
+++ CompassMobileUpdate/CompassMobileUpdate/ViewModels/ScannerViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using CompassMobileUpdate.Exceptions;
+using CompassMobileUpdate.Models;
+using CompassMobileUpdate.Services;
+
+namespace CompassMobileUpdate.ViewModels
+{
+	public class ScannerViewModel : ViewModelBase
+	{
+        private readonly MeterService _meterService;
+
+        public ScannerViewModel(MeterService meterService)
+		{
+            _meterService = meterService;
+        }
+
+        public bool IsValidSerialNumber(string serialNumber, out SerialNumberFormatException ex)
+        {
+            return _meterService.IsValidSerialNumber(serialNumber, out ex);
+        }
+
+        public async Task<Meter> GetMeterByDeviceUtilityIDAsync(string deviceUtilityID)
+        {
+            return await _meterService.GetMeterByDeviceUtilityIDAsync(deviceUtilityID);
+        }
+    }
+}
+
