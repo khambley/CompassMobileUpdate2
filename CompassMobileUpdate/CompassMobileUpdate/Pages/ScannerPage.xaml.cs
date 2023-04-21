17ec984d19731d550b4b679d44219dfda4630c27
c971939 <C971939@exelonds.com>
2023-04-21 14:30:30 -0500

    [ScannerPage] Fixed ios scanner issue

--- CompassMobileUpdate/CompassMobileUpdate/Pages/ScannerPage.xaml.cs
+++ CompassMobileUpdate/CompassMobileUpdate/Pages/ScannerPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Net.NetworkInformation;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace CompassMobileUpdate.Pages
@@ -10,6 +11,53 @@ namespace CompassMobileUpdate.Pages
         public ScannerPage()
         {
             InitializeComponent();
+
+            zxingScanner.Options.CameraResolutionPreset = ZXing.Mobile.CameraResolutionPreset.Preset1280x720;
+
+            zxingScanner.AutoFocus();
+        }
+
+        // Scanning happens on a background thread, so if you want to post something back with these results
+        // you have to make sure you're on the main thread
+        void ZXingScannerView_OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                // Stop analysis until we navigate away so we don't keep reading barcodes
+                zxingScanner.IsAnalyzing = false;
+
+                // Show an alert
+                await DisplayAlert("Scanned Barcode", result.Text, "OK");
+
+                // Navigate away
+                await Navigation.PopAsync();
+            });
+        }
+
+        void overlay_FlashButtonClicked(Xamarin.Forms.Button sender, System.EventArgs e)
+        {
+            if (zxingScanner.IsTorchOn)
+            {
+                Flashlight.TurnOffAsync();
+            }
+            else
+            {
+                Flashlight.TurnOnAsync();
+            }
+            zxingScanner.IsTorchOn = !zxingScanner.IsTorchOn;
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            zxingScanner.IsScanning = true;
+        }
+
+        protected override void OnDisappearing()
+        {
+            zxingScanner.IsScanning = false;
+
+            base.OnDisappearing();
         }
     }
 }
